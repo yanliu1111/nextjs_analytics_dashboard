166f2d998c3c78f7de982d35aa3d6609da060d11
yanliu1111 <liuyan0504@gmail.com>
2024-02-14 20:18:39 -0700

    feat: create amtVisitorsToday

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,10 +1,11 @@
 import AnalyticsDashboard from '../components/AnalyticsDashboard';
 import { analytics } from '@/utils/analytics';
+import { getDate } from '@/utils';
 
 const Page = async () => {
   const TRACKING_DAYS = 7;
 
-  const pageviews = await analytics.retrieveDays('pageview', 7);
+  const pageviews = await analytics.retrieveDays('pageview', TRACKING_DAYS);
   const totalPageviews = pageviews.reduce((acc, curr) => {
     return (
       acc +
@@ -15,11 +16,21 @@ const Page = async () => {
   }, 0);
 
   const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1); //toFixed(1) so there is only 1 decimal place
-
+  const amtVisitorsToday = pageviews
+    .filter((ev) => ev.date === getDate())
+    .reduce((acc, curr) => {
+      return (
+        acc +
+        curr.events.reduce((acc, curr) => acc + Object.values(curr)[0]!, 0)
+      );
+    }, 0);
   return (
     <div className='min-h-screen w-full py-12 flex justify-center items-center'>
       <div className='relative w-full max-w-6xl mx-auto text-white'>
-        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
+        <AnalyticsDashboard
+          avgVisitorsPerDay={avgVisitorsPerDay}
+          amtVisitorsToday={amtVisitorsToday}
+        />
       </div>
     </div>
   );
--- src/app/components/AnalyticsDashboard.tsx
+++ src/app/components/AnalyticsDashboard.tsx
@@ -4,8 +4,12 @@ import { Card } from '@tremor/react';
 
 interface AnalyticsDashboardProps {
   avgVisitorsPerDay: string; // toFixed(1) so there is string
+  amtVisitorsToday: number;
 }
-const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
+const AnalyticsDashboard = ({
+  avgVisitorsPerDay,
+  amtVisitorsToday,
+}: AnalyticsDashboardProps) => {
   return (
     <div className='flex flex-col gap-6'>
       <div className='grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6'>
@@ -15,6 +19,14 @@ const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
           </p>
           <p className='text-3xl text-dark-tremor-content-strong font-semibold'>
             {avgVisitorsPerDay}
+          </p>
+        </Card>
+        <Card className='w-fiull mx-auto max-w-xs'>
+          <p className='text-tremor-default text-dark-tremor-content'>
+            Vistor Today
+          </p>
+          <p className='text-3xl text-dark-tremor-content-strong font-semibold'>
+            {amtVisitorsToday}
           </p>
         </Card>
       </div>
